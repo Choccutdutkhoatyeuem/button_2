80bcdcf5bef8fae59c6d4a41c23fecc5cfc159db
Choccutdutkhoatyeuem <buiphankhanhan2018@gmail.com>
2024-01-06 16:59:18 +0700

    push up

--- lib/main.dart
+++ lib/main.dart
@@ -19,38 +19,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Color _buttonColor = Colors.blue;
-  void _changedColor() {
-    setState(() {
-       _buttonColor = Colors.grey;
-       });
-      Future.delayed(Duration(seconds: 1), () {
-        setState(() {
-          _buttonColor = Colors.blue;
-      });
-    });
-  }  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: ElevatedButton(
-              onPressed: _changedColor,
-              style: ElevatedButton.styleFrom(
-              primary: _buttonColor, 
-              padding: const EdgeInsets.all(20),
-            ), 
-            child: const Text(
-              'Touch me',
-              style: TextStyle(
-                color: Colors.white
-              ),
-              ),
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MyButton(),
+            const SizedBox(height: 20),
+            MyButton(),
+          ],
         ),
+      ),
     );
-  }   
+  }
 }
 
- 
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  Color _buttonColor = Colors.blue;
+
+  void _changedColor() {
+    setState(() {
+      _buttonColor = Colors.grey;
+    });
+    Future.delayed( Duration(seconds: 1), () {
+        setState(() {
+          _buttonColor = Colors.blue;
+        });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _changedColor,
+      style: ElevatedButton.styleFrom(
+        primary: _buttonColor,
+        padding: const EdgeInsets.all(20),
+      ),
+      child: const Text(
+        'Touch me',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
