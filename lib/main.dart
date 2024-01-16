4374ed135ff5b1ed29435ef6e6bf881e0cf61c43
Choccutdutkhoatyeuem <buiphankhanhan2018@gmail.com>
2024-01-16 21:58:07 +0700

    push new yeah

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:button_2/components/MyButton.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,92 +20,107 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _displayText = 'Waiting...';
-  Color _button1Color = Colors.yellow;
-  Color _button2Color = Colors.yellow;
-  Color _button3Color = Colors.yellow;
-  Color _button4Color = Colors.yellow;
+  String waitingText = "Waiting...";
+   Color buttonColor = Colors.yellow;
 
-  void _onButtonPressed(int buttonNumber) {
-    setState(() {
-      _displayText = buttonNumber == 2 ? 'True' : 'False';
-
-      _button1Color = buttonNumber == 1 ? Colors.red : Colors.yellow;
-      _button2Color = buttonNumber == 2 ? Colors.blue : Colors.yellow;
-      _button3Color = buttonNumber == 3 ? Colors.red : Colors.yellow;
-      _button4Color = buttonNumber == 4 ? Colors.red : Colors.yellow;
-    });
-    Future.delayed(Duration(seconds: 1), () {
-     setState(() {
-      _displayText ='Waiting...';
-      _button1Color = Colors.yellow;
-      _button2Color = Colors.yellow;
-      _button3Color = Colors.yellow;
-      _button4Color = Colors.yellow;
-
-     });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Pamyeuoi"),
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              _displayText,
-              style: TextStyle(fontSize: 18),
+              waitingText,
+              style: const TextStyle(fontSize: 24),
             ),
+
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _onButtonPressed(1),
-              style: ElevatedButton.styleFrom(
-                primary: _button1Color,
-                padding: const EdgeInsets.all(20),
-              ),
-              
-              child: const Text(
-                'Click Me!',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _onButtonPressed(2),
-              style: ElevatedButton.styleFrom(
-                primary: _button2Color,
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text(
-                'Click Me!',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _onButtonPressed(3),
-              style: ElevatedButton.styleFrom(
-                primary: _button3Color,
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text(
-                'Click Me!',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _onButtonPressed(4),
-              style: ElevatedButton.styleFrom(
-                primary: _button4Color,
-                padding: const EdgeInsets.all(20),
-              ),
-              child: const Text(
-                'Click Me!',
-                style: TextStyle(color: Colors.black),
-              ),
+
+            MyButton(
+              buttons: [
+                CustomButton(
+                  buttonColor: Colors.blue,
+                  content: "Click Me!",
+                  onTap: (isCorrect) {
+                    setState(() {
+                      waitingText = isCorrect ? "Correct" : "InCorrect";
+                    });
+                  },
+                  onReset: () {
+                    Future.delayed(const Duration(seconds: 1), () {
+                      setState(() {
+                        waitingText = "Waiting...";
+                      });
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 20),
+
+                CustomButton(
+                  buttonColor: Colors.red,
+                  content: "Click Me!",
+                  onTap: (isCorrect) {
+                    setState(() {
+                      waitingText = isCorrect ? "InCorrect" : "Correct";
+
+                    });
+                  },
+                  onReset: () {
+                    Future.delayed(const Duration(seconds: 1), () {
+                      setState(() {
+                        waitingText = "Waiting...";
+                      });
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 20),
+
+                CustomButton(
+                  buttonColor: Colors.red,
+                  content: "Click Me!",
+                  onTap: (isCorrect) {
+                    setState(() {
+                      waitingText = isCorrect ? "InCorrect" : "Correct";
+                    });
+                  },                 
+                  onReset: () {
+                    Future.delayed(const Duration(seconds: 1), () {
+                      setState(() {
+                        waitingText = "Waiting...";
+                      });
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 20),
+
+                CustomButton(
+                  buttonColor: Colors.red,
+                  content: "Click Me!",
+                  onTap: (isCorrect) {
+                    setState(() {
+                      waitingText = isCorrect ? "InCorrect" : "Correct";
+                      
+                    });
+                  },
+                  onReset: () {
+                    Future.delayed(const Duration(seconds: 1), () {
+                      setState(() {
+                        waitingText = "Waiting...";
+                      });
+                    });
+                  },
+                ),
+              ],
             ),
           ],
         ),
@@ -112,3 +128,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
